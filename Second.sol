9d484c74bc9d18a9a530a3ee84c48d3210cedab5
artcalling <117575698+artcalling@users.noreply.github.com>
2024-07-07 23:24:49 +0200

    Update Second.sol

--- Second.sol
+++ Second.sol
@@ -1,1 +1,31 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+contract PointManager {
+    address owner;
+    mapping(address => uint256) public userPoints;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function addPoints(address user, uint256 points) external onlyOwner {
+        userPoints[user] += points;
+    }
+
+    function changeOwner(address newOwner) external onlyOwner {
+        owner = newOwner;
+    }
+    
+    /*
+    
+      other functions
+    
+    */
+}
